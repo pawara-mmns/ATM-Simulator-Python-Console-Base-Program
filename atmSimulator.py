d851220f7ad38f0d1b5d2d7e7aa23bf0a8e717b4
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-09-04 11:26:45 +0530

    Initialize ATM simulator with account data arrays

---
+++ atmSimulator.py
@@ -0,0 +1,7 @@
+import os
+
+# Data Arrays 
+account_numbers = [1001, 1002, 1003]   # Account Numbers
+pins = [1234, 2345, 3456]              # PINs
+names = ["Kamal", "Nimal", "Sunil"]    # User names
+balances = [5000, 10000, 7500]         # Balances 
