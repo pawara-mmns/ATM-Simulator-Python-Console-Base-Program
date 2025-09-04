7c8aa3ceb750227fbd90bc8ad28f4c23859ba48e
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-09-04 11:32:40 +0530

    Refactor ATM simulator to include authenticate function and update change PINs data structure

--- atmSimulator.py
+++ atmSimulator.py
@@ -5,4 +5,14 @@ account_numbers = [1001, 1002, 1003]   # Account Numbers
 pins = [1234, 2345, 3456]              # PINs
 names = ["Kamal", "Nimal", "Sunil"]    # User names
 balances = [5000, 10000, 7500]         # Balances 
-changepins = [1111, 2222, 3333]        # Change PINs
+# changepins = [1111, 2222, 3333]        # Change PINs
+
+#  ATM Functions
+def authenticate(account, pin):
+    if account in account_numbers:
+        index = account_numbers.index(account)
+        if pins[index] == pin:
+            return True
+        return-1
+    
+
